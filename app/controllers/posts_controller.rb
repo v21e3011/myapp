3f2403cdba282d3317285f8b7d3b37279896b38c
Takeda Emu <v21e3011@oita-u.ac.jp>
2022-06-07 02:17:45 +0000

    Lesson08の課題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,10 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all.order(created_at: 'desc')
     end
+    
+    def show
+        @post = Post.find(params[:id])
+    end
+    def new
+    end
 end
