406715dfcec1266669ea7fd0beb9d13ab4f1954b
Takeda Emu <v21e3011@oita-u.ac.jp>
2022-06-16 10:54:02 +0000

    Lesson09の課題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,23 @@ class PostsController < ApplicationController
     def show
         @post = Post.find(params[:id])
     end
-    def new
+    def create
+        #    render plain: "受信パラメータ：#{params}"
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to posts_path
+        else
+            render "new"
+        end
     end
+    
+    def new
+        @post = Post.new
+    end
+    
+private
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
+
 end
